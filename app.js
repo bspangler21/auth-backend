f48cf2dc1ded679711014e12d0c95b68627ee815
bspang <bspangler21@alumni.uwosh.edu>
2024-11-09 12:36:02 -0600

    Add login endpoint with JWT authentication

--- app.js
+++ app.js
@@ -2,6 +2,7 @@ const express = require("express");
 const app = express();
 const bodyParser = require("body-parser");
 const bcrypt = require("bcrypt");
+const jwt = require("jsonwebtoken");
 const User = require("./db/userModel");
 
 // body parser configuration
@@ -36,6 +37,53 @@ app.post("/register", (request, response) => {
 		.catch((error) => {
 			response.status(500).send({
 				message: "Password was not hashed successfully",
+				error,
+			});
+		});
+});
+
+app.post("/login", (request, response) => {
+	//Check that user exists
+	User.findOne({ email: request.body.email })
+		.then((user) => {
+			bcrypt
+				.compare(request.body.password, user.password)
+        //Do this if the passwords match
+				.then((passwordCheck) => {
+					//Check if password matches
+					if (!passwordCheck) {
+						return response.status(400).send({
+							message: "Email or password do not match",
+						});
+					}
+
+					//Create JWT token
+					const token = jwt.sign(
+						{
+							userId: user._id,
+							userEmail: user.email,
+						},
+						"RANDOM-TOKEN",
+						{ expiresIn: "24h" }
+					);
+
+					//Return success response
+					response.status(200).send({
+						message: "Login successful",
+						email: user.email,
+						token,
+					});
+				})
+				.catch((error) => {
+					response.status(400).send({
+						message: "Email or password do not match",
+						error,
+					});
+				});
+		})
+		.catch((error) => {
+			response.status(404).send({
+				message: "User not found",
 				error,
 			});
 		});
